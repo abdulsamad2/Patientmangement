202d93dfdbe8c1a1e7787c516223c62d392a7900
AbdulSamad <abdulsamad.laghari1@gmail.com>
2024-07-07 12:01:45 +0500

    uodate

--- Backend/model/doctorModel.js
+++ Backend/model/doctorModel.js
@@ -31,29 +31,10 @@ const doctorSchema = new mongoose.Schema({
     required: true,
     trim: true,
   },
-<<<<<<< HEAD
   specialties: {
     type: String,
-    required: true,
   },
 
-  education: {
-    type: String,
-    required: true,
-  },
-
-  workExperience: {
-    type: String,
-    required: true,
-  },
-
-=======
-  specialties:{
-    type:String
-  },
-
- 
->>>>>>> 043fed7b483c99d3a03cba7b46256ecf2c562449
   licenseNumber: {
     type: String,
     required: true,
--- Frontend/src/components/DoctorForm.tsx
+++ Frontend/src/components/DoctorForm.tsx
@@ -28,38 +28,32 @@ type Inputs = {
 export function DoctorForm() {
   const formSchema = z.object({
     name: z.string().min(2, {
-<<<<<<< HEAD
-=======
       message: "Username must be at least 3 characters.",
     }),
-    email: z.string().email( {
->>>>>>> 043fed7b483c99d3a03cba7b46256ecf2c562449
+    email: z.string().email({
       message: "Username must be at least 2 characters.",
     }),
-    phone: z.string().min(11,{
-      message:"Please provide correct mobile number"
+    phone: z.string().min(11, {
+      message: "Please provide correct mobile number",
     }),
-    gender:z.string({
-      message:"Please Provide correct gender "
+    gender: z.string({
+      message: "Please Provide correct gender ",
     }),
-    dateOfbirth : z.date({
-      message:"please provide dete of birth"
+    dateOfbirth: z.date({
+      message: "please provide dete of birth",
     }),
-    address:z.string({
-      message:"please provide a valid address"
+    address: z.string({
+      message: "please provide a valid address",
     }),
     specialtity: z.string({
-      message:"please Provide a valid speciality"
+      message: "please Provide a valid speciality",
     }),
-  licenseNumber: z.string({
-    message:"please privde a valid licence number"
-  }),
-  bio:z.string().min(20,{
-    message:"bio must be atleast 20 chracters"
-
-  }
-  )
-
+    licenseNumber: z.string({
+      message: "please privde a valid licence number",
+    }),
+    bio: z.string().min(20, {
+      message: "bio must be atleast 20 chracters",
+    }),
   });
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -71,147 +65,6 @@ export function DoctorForm() {
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
-<<<<<<< HEAD
-    <div className="max-w-screen-sm">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <div className="grid grid-cols-2 gap-6">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="John Doe" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email Address</FormLabel>
-                  <FormControl>
-                    <Input placeholder="example@example.com" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-
-          <div className="grid grid-cols-2 gap-6">
-            <FormField
-              control={form.control}
-              name="phone"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Phone</FormLabel>
-                  <FormControl>
-                    <Input placeholder="+1 213 456 7890" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="gender"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Gender</FormLabel>
-                  <FormControl>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select Gender" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="male">Male</SelectItem>
-                        <SelectItem value="female">Female</SelectItem>
-                        <SelectItem value="Other">Other</SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-
-          <div className="grid grid-cols-2 gap-6">
-            <FormField
-              control={form.control}
-              name="dateofbirth"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Date of Birth</FormLabel>
-                  <FormControl>
-                    <Input type="date" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="address"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Address</FormLabel>
-                  <FormControl>
-                    <Input placeholder="123 Main St" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="grid grid-cols-2 gap-6">
-            <FormField
-              control={form.control}
-              name="specialties"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Specialty</FormLabel>
-                  <FormControl>
-                    <Input type="text" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="education"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Education</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="MBBS, MCPS (Obstetrics & Gynaecology),"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
-    </div>
-=======
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
@@ -226,9 +79,8 @@ export function DoctorForm() {
               <FormMessage />
             </FormItem>
           )}
-          
         />
-         <FormField
+        <FormField
           control={form.control}
           name="email"
           render={({ field }) => (
@@ -240,9 +92,8 @@ export function DoctorForm() {
               <FormMessage />
             </FormItem>
           )}
-          
         />
-         <FormField
+        <FormField
           control={form.control}
           name="phone"
           render={({ field }) => (
@@ -254,9 +105,8 @@ export function DoctorForm() {
               <FormMessage />
             </FormItem>
           )}
-          
         />
-         <FormField
+        <FormField
           control={form.control}
           name="gender"
           render={({ field }) => (
@@ -268,14 +118,10 @@ export function DoctorForm() {
               <FormMessage />
             </FormItem>
           )}
-          
         />
-        
-
 
         <Button type="submit">Submit</Button>
       </form>
     </Form>
->>>>>>> 043fed7b483c99d3a03cba7b46256ecf2c562449
   );
 }
